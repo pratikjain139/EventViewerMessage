ef94ffa3156149192de81c91dac34fa40db32f7f
pratikjain139 <jp13986@gmail.com>
2018-03-03 07:21:43 +0530

    Initial commit

---
+++ EventViewerMessage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventViewerMessage
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            for(int i =0;i<args.Length; i++)
+            {
+                MessageBox.Show(args[i]);
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);            
+        }
+    }
+}
